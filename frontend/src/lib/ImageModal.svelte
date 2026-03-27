b43e5fa618912d78ba4d0302b08daed99f703a66
Daniel Heise <daniel@familie-heise.de>
2026-03-27 15:02:45 +0100

    fix: wrap image in key block to trigger transition on change

--- frontend/src/lib/ImageModal.svelte
+++ frontend/src/lib/ImageModal.svelte
@@ -98,12 +98,14 @@
   >
     <div class="relative max-h-full max-w-full" in:scale={{ start: 0.9, duration: 150 }} out:fade={{ duration: 100 }}>
       {#if currentBlobUrl}
-        <img
-          class="max-h-[85vh] max-w-[85vw] rounded-lg object-contain shadow-2xl"
-          src={currentBlobUrl}
-          alt={currentAttachment?.filename ?? ''}
-          in:fly={{ x: direction === 'right' ? 40 : -40, duration: 350, opacity: 0, easing: (t) => 1 - Math.pow(1 - t, 3) }}
-        />
+        {#key currentIndex}
+          <img
+            class="max-h-[85vh] max-w-[85vw] rounded-lg object-contain shadow-2xl"
+            src={currentBlobUrl}
+            alt={currentAttachment?.filename ?? ''}
+            in:fly={{ x: direction === 'right' ? 40 : -40, duration: 350, opacity: 0, easing: (t) => 1 - Math.pow(1 - t, 3) }}
+          />
+        {/key}
       {:else if error}
         <div class="flex h-48 w-48 items-center justify-center rounded-lg bg-white/10">
           <div class="text-sm text-red-400">{error}</div>
